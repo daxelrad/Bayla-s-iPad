ffe8b4f887ab421540ed973ea966820cdfa53a6f
David Axelrad <daxelrad@rogers.com>
2016-04-30 19:34:03 -0400

    Initial Commit

---
+++ Bayla's iPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bayla's iPad
+//
+//  Created by David Axelrad on 2016-04-30.
+//  Copyright © 2016 Bayla Gross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Bayla's iPadTests/Bayla_s_iPadTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bayla_s_iPadTests.swift
+//  Bayla's iPadTests
+//
+//  Created by David Axelrad on 2016-04-30.
+//  Copyright © 2016 Bayla Gross. All rights reserved.
+//
+
+import XCTest
+@testable import Bayla_s_iPad
+
+class Bayla_s_iPadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
